e7047bfc0a43c5473a2177d4497bf5d0573b8934
Fabrizio <faguebert@gmail.com>
2022-10-11 14:48:03 -0300

    Mapeando classe incorporável Endereco

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -78,13 +78,13 @@ public class RestauranteController {
 	@PutMapping("/{restauranteId}")
 	public ResponseEntity<?> atualizar(@PathVariable Long restauranteId, @RequestBody Restaurante restaurante){
 		try {
-			Optional<Restaurante> restauranteAtual = restauranteRepository.findById(restauranteId);
+			Restaurante restauranteAtual = restauranteRepository.findById(restauranteId).orElse(null);
 			
-			if(restauranteAtual.isPresent()) {
-				BeanUtils.copyProperties(restaurante, restauranteAtual, "id");
+			if(restauranteAtual != null) {
+				BeanUtils.copyProperties(restaurante, restauranteAtual, "id", "formasPagamento");
 				
-				Restaurante restauranteAtualizado = cadastroRestaurante.salvar(restauranteAtual.get());
-				return ResponseEntity.ok(restauranteAtualizado);
+				restauranteAtual = cadastroRestaurante.salvar(restauranteAtual);
+				return ResponseEntity.ok(restauranteAtual);
 			}
 			
 			return ResponseEntity.notFound().build();
