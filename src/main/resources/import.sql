fdb1c8ba9b584869c56d61c27b9ac9f7040ceaff
Fabrizio <faguebert@gmail.com>
2022-10-11 15:03:29 -0300

    Ajustes nos endpoint com o Endereco dos restaurantes

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,15 +1,15 @@
 insert into cozinha (id, nome_cozinha) values (1, 'Tailandesa');
 insert into cozinha (id, nome_cozinha) values (2, 'Indiana');
 
-insert into restaurante (id, nome_restaurante, taxa_frete, cozinha_id) values (1, 'Thai Gourmet', 10, 1);
-insert into restaurante (id, nome_restaurante, taxa_frete, cozinha_id) values (2, 'Thai Delivery', 9.50, 1);
-insert into restaurante (id, nome_restaurante, taxa_frete, cozinha_id) values (3, 'Tuk Tuk Comida Indiana', 15, 2);
-
 insert into estado (id, nome_estado) values (1, 'Paraná');
 insert into estado (id, nome_estado) values (2, 'São Paulo');
- 
+
 insert into cidade(id, nome_cidade, estado_id) values (1, 'Curitiba', 1);
 insert into cidade(id, nome_cidade, estado_id) values (2, 'Sorocaba', 2);
+
+insert into restaurante (id, nome_restaurante, taxa_frete, cozinha_id, endereco_cidade_id, endereco_cep, endereco_logradouro, endereco_numero, endereco_bairro) values (1, 'Thai Gourmet', 10, 1, 1, '38400-999', 'Rua João Pinheiro', '1000', 'Centro');
+insert into restaurante (id, nome_restaurante, taxa_frete, cozinha_id) values (2, 'Thai Delivery', 9.50, 1);
+insert into restaurante (id, nome_restaurante, taxa_frete, cozinha_id) values (3, 'Tuk Tuk Comida Indiana', 15, 2);
 
 insert into forma_pagamento (id, descricao_forma_pagamento) values (1, 'Cartão de crédito');
 insert into forma_pagamento (id, descricao_forma_pagamento) values (2, 'Cartão de débito');
