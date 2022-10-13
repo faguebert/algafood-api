881ae6fbc8917a32e555288158d2c08b4df37273
Fabrizio <faguebert@gmail.com>
2022-10-13 16:18:51 -0300

    Eager e lazy loading

--- src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -2,6 +2,7 @@ package com.algaworks.algafood.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.algaworks.algafood.domain.model.Restaurante;
@@ -12,5 +13,7 @@ import java.util.List;
 @Repository
 public interface RestauranteRepository extends CustomJpaRepository<Restaurante, Long>, JpaSpecificationExecutor<Restaurante> {
 
+    @Query("from Restaurante r join r.cozinha join fetch r.formasPagamento")
+    List<Restaurante> findAll();
     List<Restaurante> find(String nomeRestaurante, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal);
 }
